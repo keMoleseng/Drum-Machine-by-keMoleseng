294617d496fa77081c0821692f5313cd299122e9
keMoleseng <101043763+keMoleseng@users.noreply.github.com>
2022-03-29 14:18:42 +0200

    Add files via upload
    add jQuery JS and soundOnClick JS.

---
+++ js/sound-onclick.js
@@ -0,0 +1,150 @@
+// clap sound-onclick
+function playclip() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "clap-snare.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("Q");
+audio.play();
+}
+}
+};
+
+function playclip2() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "clap2.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("W");
+audio.play();
+}
+}
+};
+
+function playclip3() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "clap7.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("E");
+audio.play();
+}
+}
+};
+
+
+// fx sound-onclick
+function playclip4() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "fall_fx3.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("A");
+audio.play();
+}
+}
+};
+
+function playclip5() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "fall_fx5.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("S");
+audio.play();
+}
+}
+};
+
+function playclip6() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "rise_fx3.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("D");
+audio.play();
+}
+}
+};
+
+
+//hardkick sound-onclick
+function playclip7() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "hard_kick_1.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("Z");
+audio.play();
+}
+}
+};
+
+function playclip8() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "hard_kick_2.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("X");
+audio.play();
+}
+}
+};
+
+function playclip9() {
+if (navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.indexOf("MSIE 7")!=-1) || (navigator.appVersion.indexOf("MSIE 8")!=-1)) {
+if (document.all)
+ {
+  document.all.sound.src = "hard_kick_3.mp3";
+ }
+}
+
+else {
+{
+var audio = document.getElementById("C");
+audio.play();
+}
+}
+};
+
+
